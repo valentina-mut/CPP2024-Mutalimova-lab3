7a738778e5dd052cfbc87b3cbb281d1556d0c79f
valentina-mut <159520924+valentina-mut@users.noreply.github.com>
2024-05-23 10:16:07 +0300

    CPP2024-lab3 Mutalimova Valentina 5030102/20001

---
+++ Gems/build/CMakeFiles/Game.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/royalist/Рабочий стол/c++/Язык_кодирования_С++/GEMS (копия)/Gems/src/Board.cpp" "CMakeFiles/Game.dir/src/Board.cpp.o" "gcc" "CMakeFiles/Game.dir/src/Board.cpp.o.d"
+  "/home/royalist/Рабочий стол/c++/Язык_кодирования_С++/GEMS (копия)/Gems/src/Bonus.cpp" "CMakeFiles/Game.dir/src/Bonus.cpp.o" "gcc" "CMakeFiles/Game.dir/src/Bonus.cpp.o.d"
+  "/home/royalist/Рабочий стол/c++/Язык_кодирования_С++/GEMS (копия)/Gems/src/Gem.cpp" "CMakeFiles/Game.dir/src/Gem.cpp.o" "gcc" "CMakeFiles/Game.dir/src/Gem.cpp.o.d"
+  "/home/royalist/Рабочий стол/c++/Язык_кодирования_С++/GEMS (копия)/Gems/src/main.cpp" "CMakeFiles/Game.dir/src/main.cpp.o" "gcc" "CMakeFiles/Game.dir/src/main.cpp.o.d"
+  "/home/royalist/Рабочий стол/c++/Язык_кодирования_С++/GEMS (копия)/Gems/src/moveGem.cpp" "CMakeFiles/Game.dir/src/moveGem.cpp.o" "gcc" "CMakeFiles/Game.dir/src/moveGem.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Gems/build/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game.dir/src/Board.cpp.o"
+  "CMakeFiles/Game.dir/src/Board.cpp.o.d"
+  "CMakeFiles/Game.dir/src/Bonus.cpp.o"
+  "CMakeFiles/Game.dir/src/Bonus.cpp.o.d"
+  "CMakeFiles/Game.dir/src/Gem.cpp.o"
+  "CMakeFiles/Game.dir/src/Gem.cpp.o.d"
+  "CMakeFiles/Game.dir/src/main.cpp.o"
+  "CMakeFiles/Game.dir/src/main.cpp.o.d"
+  "CMakeFiles/Game.dir/src/moveGem.cpp.o"
+  "CMakeFiles/Game.dir/src/moveGem.cpp.o.d"
+  "Game"
+  "Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
